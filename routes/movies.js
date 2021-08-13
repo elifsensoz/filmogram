fcedafee31d3f6da1d91282d1f8a05b301817d60
elifsensoz <elfsensoz@gmail.com>
2021-08-13 21:56:59 +0300

    Catch fix2

--- routes/movies.js
+++ routes/movies.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
             movies: movies,
             searchOptions: req.query})
         
-    } catch(err){
+    } catch{
         res.redirect('/')
     }
     
